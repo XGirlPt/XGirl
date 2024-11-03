5758c2e11e54f85b4e8b665b88229ee765d1725e
Mike Pine <info@xgirl.pt>
2024-11-03 12:29:00 +0100

    .

--- src/components/Maiores.tsx
+++ src/components/Maiores.tsx
@@ -9,12 +9,9 @@ const Maiores: React.FC<MaioresProps> = ({ setShowMaiores }) => {
   const [mostrarMaiores, setMostrarMaiores] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setMostrarMaiores(true), 50); // Pequeno atraso para evitar layout shift
-    document.body.classList.add("overflow-hidden", "h-screen");
-  
+    document.body.style.overflow = "hidden";
     return () => {
-      clearTimeout(timer);
-      document.body.classList.remove("overflow-hidden", "h-screen");
+      document.body.style.overflow = "";
     };
   }, []);
 
@@ -26,19 +23,15 @@ const Maiores: React.FC<MaioresProps> = ({ setShowMaiores }) => {
   return (
     <>
       {mostrarMaiores && (
-        <div className={`fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-75 backdrop-blur-sm transition-opacity duration-300 ${mostrarMaiores ? 'opacity-100' : 'opacity-0'}`}>
-  <div className="w-full max-w-lg h-[400px] p-8 md:p-12 bg-[#1E2427] rounded-lg shadow-2xl space-y-4">
-  <h1 className="text-xl font-bold text-white">
-              Aviso de Conteúdo Adulto e Uso de Cookies
-            </h1>
+        <div className="fixed inset-0 z-50 flex justify-center items-center bg-black bg-opacity-75 backdrop-blur-sm transition-opacity duration-300">
+          <div className="w-full max-w-lg h-[400px] p-8 md:p-12 bg-[#1E2427] rounded-lg shadow-2xl space-y-4">
+            <h1 className="text-xl font-bold text-white">Aviso de Conteúdo Adulto e Uso de Cookies</h1>
             <p className="text-white">Bem-vindo ao Xgirl.pt</p>
             <p className="text-white text-sm">
-              Este website contém materiais restritos por idade, incluindo
-              nudez. Ao entrar, você confirma que tem pelo menos 18 anos de
-              idade ou a idade da maioria na jurisdição de onde está acessando o
-              website, e consente em visualizar conteúdo sexualmente explícito.
+              Este website contém materiais restritos por idade, incluindo nudez.
+              Ao entrar, você confirma que tem pelo menos 18 anos de idade ou a idade da maioria na
+              jurisdição de onde está acessando o website, e consente em visualizar conteúdo sexualmente explícito.
             </p>
-
             <div className="flex justify-around mt-4 pt-10 gap-6">
               <button
                 className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 text-sm"
